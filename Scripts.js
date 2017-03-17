5cdbcf111bad8c386baa7b90302fe1ba92636a1c
IAShulzhenko <shulzhenko.ivan.andreevich@gmail.com>
2017-03-17 17:35:38 +0300

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,22 +1,21 @@
-$(document).ready(function () {
-
 function currentTime(){
 	var d = new Date();
 	var h = d.getHours();
 	var m = d.getMinutes();
 	var s = d.getSeconds();
+	var hh = document.getElementById("HH");
+	var mm = document.getElementById("MM");
+	var ss = document.getElementById("SS");
 	if (h<10){
-		$('#HH').html("0" + h);
-	}else $('#HH').html(h);
+		hh.innerHTML = "0" + h;
+	}else hh.innerHTML = h;
 	if (m<10){
-		$('#MM').html("0" + m);
-	}else $('#MM').html(m);
+		mm.innerHTML = "0" + m;
+	}else mm.innerHTML = m;
 	if (s<10){
-		$('#SS').html("0" + s);
-	}else $('#SS').html(s);
-	setInterval(currentTime, 1000);
-}	
+		ss.innerHTML = "0" + s;
+	}else ss.innerHTML = s;
+	setInterval(currentTime, 1000)
+}
 
 currentTime();
-
-});
